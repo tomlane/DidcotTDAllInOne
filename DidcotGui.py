0731a03a23e243b5a3918701aeda0f6aa53029ed
stever87 <furi_d@hotmail.com>
2016-10-13 17:13:19 +0100

    more gui added

--- DidcotGui.py
+++ DidcotGui.py
@@ -1,9 +1,8 @@
 from Berth import Berth
 
-tile = '-'
+
 
 class DidcotGui(object):
-
 
     def __init__(self, win):
       self.b1014 = Berth('1014', 30, 1)
@@ -18,8 +17,8 @@ class DidcotGui(object):
       self.b0966 = Berth('0966', 30, 68)
       self.b0965 = Berth('0965', 30, 75)
       self.b0952 = Berth('0952', 30, 82)
-      self.b0949 = Berth('0949', 30, 89)
-      self.b0947 = Berth('0947', 32, 89)
+      self.b0949 = Berth('0949', 30, 92)
+      self.b0947 = Berth('0947', 32, 92)
       self.b0950 = Berth('0950', 32, 82)
       self.b0963 = Berth('0963', 32, 75)
       self.b0964 = Berth('0964', 32, 68)
@@ -32,7 +31,7 @@ class DidcotGui(object):
       self.b0999 = Berth('0999', 32, 22)
       self.b1003 = Berth('1003', 32, 15)
       self.b1007 = Berth('1007', 32, 7)
-      self.b0951 = Berth('0951', 28, 89)
+      self.b0951 = Berth('0951', 28, 92)
       self.b0954 = Berth('0954', 28, 82)
       self.b0961 = Berth('0961', 34, 75)
       self.b0987 = Berth('0987', 34, 33)
@@ -57,11 +56,11 @@ class DidcotGui(object):
       self.b0918 = Berth('0918', 15, 50)
       self.b0920 = Berth('0920', 13, 50)
       self.b0931 = Berth('0931', 23, 30)
-      self.b0941 = Berth('0941', 23, 13)
-      self.b0936 = Berth('0936', 23, 7)
-      self.b0938 = Berth('0938', 21, 7)
-      self.b0940 = Berth('0940', 19, 7)
-      self.b0942 = Berth('0942', 17, 7)
+      self.b0941 = Berth('0941', 23, 10)
+      self.b0936 = Berth('0936', 23, 5)
+      self.b0938 = Berth('0938', 21, 5)
+      self.b0940 = Berth('0940', 19, 5)
+      self.b0942 = Berth('0942', 17, 5)
       self.b0921 = Berth('0921', 19, 45)
       self.b0923 = Berth('0923', 17, 45)
       self.b0925 = Berth('0925', 15, 45)
@@ -69,13 +68,12 @@ class DidcotGui(object):
       self.b0926 = Berth('0926', 17, 33)
       self.b0933 = Berth('0933', 19, 27)
       self.b0935 = Berth('0935', 17, 27)
-      self.b0944 = Berth('0944', 17, 15)
       self.b6420 = Berth('6240', 19, 15)
       self.b0922 = Berth('0922', 3, 47)
-      self.b2209 = Berth('2209', 5, 20)
+      self.b2209 = Berth('2209', 5, 22)
       self.b2207 = Berth('2207', 6, 22)
-      self.b2208 = Berth('2208', 7, 20)
-      self.b2205 = Berth('2205', 9, 20)
+      self.b2208 = Berth('2208', 7, 22)
+      self.b2205 = Berth('2205', 9, 22)
       self.b2201 = Berth('2201', 13, 21)
       self.b0937 = Berth('0937', 15, 23)
       self.b0928 = Berth('0928', 7, 33)
@@ -97,7 +95,7 @@ class DidcotGui(object):
         self.b0906, self.b0907, self.b0904, self.b0905, self.b0911, self.b0931 , self.b0941,
         self.b0913, self.b0912, self.b0914, self.b0916, self.b0918, self.b0920, self.b0936, 
         self.b0938, self.b0940, self.b0942, self.b0921, self.b0923, self.b0925, self.b0924, 
-        self.b0926, self.b0933, self.b0935, self.b0944, self.b6420, self.b0922, self.b2209,
+        self.b0926, self.b0933, self.b0935, self.b6420, self.b0922, self.b2209,
         self.b2207, self.b2208, self.b2205, self.b2201, self.b0937, self.b0928, self.b2203,
         self.b6416, self.b6413, self.b6411, self.b2210, self.b2211, self.b2212, self.b2214]
       
@@ -106,6 +104,8 @@ class DidcotGui(object):
 
 
     def drawBerths(self, berths):
+
+
 
       berths = berths
 
@@ -124,23 +124,63 @@ class DidcotGui(object):
 
     def drawFixedMap(self):
 
+      tile = '-'
+      point1 = '/'
+      point2 = '\\'
+      vertical = '|'
+
       self.win.keypad(1)
       self.win.border(0)
       self.win.nodelay(1)
 
       self.win.addstr(0, 2, 'Didcot TD')
 
+      #lines
+
       self.win.addstr(30, 1, tile * 98) #Up Main
       self.win.addstr(21, 1, tile * 98) #Up Main
       self.win.addstr(32, 1, tile * 98) #Dn Main
       self.win.addstr(23, 1, tile * 98) #Dn Main
       self.win.addstr(19, 1, tile * 98) #Dn Relief
-      self.win.addstr(17, 27, tile * 72) #Up Relief
+      self.win.addstr(17, 17, tile * 82) #Up Relief
       self.win.addstr(34, 15, tile * 30) #Dn Relief Wantage
       self.win.addstr(28, 15, tile * 30) #Up Relief Wantage
       self.win.addstr(34, 74, tile * 13) #Dn Milton Loop
       self.win.addstr(28, 74, tile * 25) #Up Milton Loop
       self.win.addstr(3, 1, tile * 53) #Up Oxford / Avoider
       self.win.addstr(5, 1, tile * 53) #Dn Oxford / Avoider
+      self.win.addstr(7, 20, tile * 20) #Up Chester
+      self.win.addstr(9, 20, tile * 20) #Dn Chester
+      self.win.addstr(15, 42, tile * 14) #P5
+
+      #points
+
+      self.win.addstr(29, 14, point1) #UM/UR Challow
+      self.win.addstr(29, 45, point2) #UR/UM Wantage
+      self.win.addstr(33, 14, point2) #DR/DM Challow
+      self.win.addstr(33, 45, point1) #DM/DR Wantage
+      self.win.addstr(29, 73, point1) #UM/UR Milton
+      self.win.addstr(33, 73, point2) #DG/DM Milton
+      self.win.addstr(33, 87, point1) #DM/DG Milton
+      self.win.addstr(31, 88, point1) #DM/UM Milton
+      self.win.addstr(29, 89, point1) #UM/UR Milton2
+      self.win.addstr(20, 12, point1) #UM/UR Foxhall
+      self.win.addstr(20, 20, point1) #UM/UR Foxhall
+      self.win.addstr(22, 15, point1) #DM/UM Foxhall
+      self.win.addstr(22, 25, point2) #DM/UM Foxhall
+      self.win.addstr(18, 19, point1) #UR/UG Foxhall
+      self.win.addstr(18, 22, point1) #UR/UG Foxhall
+      self.win.addstr(18, 86, point1) #DR/UP Moreton Cutting
+      self.win.addstr(20, 85, point1) #UM/DR Moreton Cutting
+      self.win.addstr(22, 84, point1) #UM/DM Moreton Cutting
+      self.win.addstr(22, 70, point2) #UM/DM Didcot East
+      self.win.addstr(18, 70, point2) #UR/DR Didcot East
+      self.win.addstr(20, 68, point2) #DR/UM Didcot East
+      self.win.addstr(20, 72, point2) #DR/UM Didcot East
+      self.win.addstr(18, 60, point2) #DR/UR Didcot East
+      self.win.addstr(16, 58, point2) #DO/DR Didcot East
+      self.win.addstr(16, 62, point2) #UO/UR Didcot East
+      self.win.addstr(16, 56, point2) #P5/UR
+      self.win.addstr(18, 39, point1) #UR/P4 Station
 
       self.win.timeout(200) 
